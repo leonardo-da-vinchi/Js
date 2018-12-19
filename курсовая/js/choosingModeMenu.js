bca8404ad8086d54f1a0e3ce12a7efcd4a1d8a88
Brodicry <33390927+Brodicry@users.noreply.github.com>
2018-12-19 19:43:13 +0300

    ssds

--- курсовая/js/choosingModeMenu.js
+++ курсовая/js/choosingModeMenu.js
@@ -1,39 +1,45 @@
 let selectMenu = $("select");
-selectMenu.on("change", function(){
+selectMenu.on("change", function () {
     switch ($(this).val()) {
         case "rus-ez":
-        text = new RusEz(funcForRandom, text).getText();
-        break;
+            text = new RusEz(funcForRandom, text).getText();
+            break;
 
         case "rus-norm":
-        text = new RusNorm(funcForRandom, text).getText();
-        break;
+            text = new RusNorm(funcForRandom, text).getText();
+            break;
 
         case "rus-hard":
-        text = new RusHard(funcForRandom, text).getText();
-        break;
+            text = new RusHard(funcForRandom, text).getText();
+            break;
 
         case "eng-ez":
-        text = new EngEz(funcForRandom, text).getText();
-        break;
+            text = new EngEz(funcForRandom, text).getText();
+            break;
 
         case "eng-norm":
-        text = new EngNorm(funcForRandom, text).getText();
-        break;
+            text = new EngNorm(funcForRandom, text).getText();
+            break;
 
         case "eng-hard":
-        text = new EngHard(funcForRandom, text).getText();
-        break;
+            text = new EngHard(funcForRandom, text).getText();
+            break;
     }
-    text = formatText(text); 
+    text = formatText(text);
     useText = getUseText(text);
-    $(".textarea").html('<mark>'+useText[0]+'</mark>'+useText.slice(1,useText.length));
+    leftText = text.slice(0, useText.length);
+    $(".textarea").html('<mark>' + useText[0] + '</mark>' + useText.slice(1, useText.length));
     $("main header p:first-child").html("Можете начинать печатать");
     $("main header p mark").css("display", "inline");
-    $(".play-mode img").css("display", "block");  
+    $(".play-mode img").css("display", "block");
+    $("body").on("keydown", textInputing);
+    $("body").on("keypress", function(event){
+        alert(String.fromCharCode(event.charCode)==useText[0]);
     });
-
-    $(".play-mode img").on("click", function(){
+    $(".play-mode img").on("click", function () {
         selectMenu.trigger("change");
     });
+});
+
+
 
---
+++ курсовая/js/textInputing.js
@@ -0,0 +1,3 @@
+function textInputing(event){
+    // alert(event.keyCode);
+}
